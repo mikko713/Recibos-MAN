b4e1cb333d54ee7e88f78cd8eaf8e0e882c8fc00
mikko713 <cachemir.bateadores_64@icloud.com>
2025-08-27 23:47:26 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='recibos-mc-v2'; const ASSETS=['./','./index.html','./manifest.webmanifest','./service-worker.js','./icon-192.png','./icon-512.png']; self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();}); self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim()));}); self.addEventListener('fetch',e=>{const url=new URL(e.request.url); if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));}});
+const CACHE='recibos-mc-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./service-worker.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{const url=new URL(e.request.url); if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));}});
